5ff344cb407c423e1b03c9a2fc3054277665b8e0
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2023-02-22 00:01:41 +0300

    Anasayfa slider eklendi strapi bağlantısı useFetch hook'u ile sağlandı.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,38 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import useFetch from "../hooks/useFetch";
 const HomePage = () => {
+
+  const { isLoading, error, data } = useFetch(
+    "http://localhost:1337/api/home-page-main-sliders"
+  );
+
+  if (isLoading) return <h1>Yükleniyor...</h1>;
+  if (error) return <h1>Hata: {error.message}</h1>;
+
+  console.log(data);
   return (
     <section className='HomePage'>
-      <div className='container'>
+      <div className='container-fluid'>
         <div className='row'>
-          <div className='col-lg-12'>
-          HomePage
+          <div className='col-lg-6'>
+          <Swiper
+      spaceBetween={10}
+      slidesPerView={1}
+
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {data.data.map((item) => (
+     
+      <SwiperSlide key={item.id}><img className='img-fluid' src={item.attributes.Mainsliderlink}></img></SwiperSlide>
+
+      ))}
+    </Swiper>
           </div>
         </div>
       </div>
     </section>
+    
   )
 }
 
---
+++ src/components/Swiperitem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Swiperitem = ({imageSrc, imageAlt}) => {
+  return (
+   <li className='swiper-item'>
+    <img  src='' alt='' className='swiper-img'/>
+   </li>
+  )
+}
+
+export default Swiperitem
