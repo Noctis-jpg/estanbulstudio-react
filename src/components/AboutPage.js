67176f8489233d97be3287c92ddacacd5654b966
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2023-03-11 22:37:14 +0300

    Hizmet kartları slide ve Strapiden çekiyor.
    Hizmet kartları slide ve Strapiden çekiyor.

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const AboutPage = () => {
+
   return (
-    <section className='PageAbout'>
+    <section className='PageContact'>
     <div className='container'>
       <div className='row'>
         <div className='col-lg-12'>
-        AboutPage
+        ContactPage
         </div>
       </div>
     </div>
--- src/components/section_components/ServicesSection.js
+++ src/components/section_components/ServicesSection.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 import useFetch from "../../hooks/useFetch";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import { Swiper, SwiperSlide } from "swiper/react";
+import Card from "react-bootstrap/Card";
+import { Pagination } from "swiper";
+import SwiperCore, { Autoplay } from "swiper";
+import "swiper/css/pagination";
+import { Link } from 'react-router-dom';
 const ServicesSection = () => {
   const { isLoading, error, data } = useFetch(
     "http://localhost:1337/api/card-serviss"
@@ -9,65 +13,88 @@ const ServicesSection = () => {
 
   if (isLoading) return <h1>Yükleniyor...</h1>;
   if (error) return <h1>Hata: {error.message}</h1>;
+  SwiperCore.use([Autoplay]);
+  console.log(data);
   return (
-    <div className='container servicesSection'>
+    <div className="container-fluid servicesSection">
       {/* <div className='verticalLine'></div> */}
-    <div className='row'>
-      <div className='col-lg-6'>
-      <Card>
-      <Card.Img className='ymServicesimg1' variant="top" src="http://localhost:1337/uploads/portrait_of_an_asia_girl_NCDLHZE_8108fe01af.jpg?updated_at=2023-02-26T11:29:12.208Z" />
-      <Card.Body>
-        <Card.Title>Kameralar</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        {/* <Button variant="primary">Go somewhere</Button> */}
-      </Card.Body>
-    </Card>
-      </div>
-      <div className='col-lg-6'>
-      <Card>
-      <Card.Img className='ymServicesimg' variant="top" src="http://localhost:1337/uploads/portrait_of_an_asia_girl_6_CCZHG_6_819x1024_71c93a1fcb.jpg?updated_at=2023-02-26T12:43:25.197Z" />
-      <Card.Body>
-        <Card.Title>Lensler</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        {/* <Button variant="primary">Go somewhere</Button> */}
-      </Card.Body>
-    </Card>
-      </div>
-      <div className='col-lg-6'>
-      <Card>
-      <Card.Img className='ymServicesimg1' variant="top" src="http://localhost:1337/uploads/portrait_of_an_asia_girl_6_KUHQXZ_819x1024_659638cc8d.jpg?updated_at=2023-02-26T12:54:19.648Z" />
-      <Card.Body>
-        <Card.Title>Işıklar</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        {/* <Button variant="primary">Go somewhere</Button> */}
-      </Card.Body>
-    </Card>
-      </div>
-      <div className='col-lg-6'>
-      <Card>
-      <Card.Img className='ymServicesimg2' variant="top" src="http://localhost:1337/uploads/portrait_of_an_asia_beautiful_girl_A3_EB_9_AP_f079f61133.jpg?updated_at=2023-02-26T12:54:49.112Z" />
-      <Card.Body>
-        <Card.Title>Teknik Ekipmanlar</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        {/* <Button variant="primary">Go somewhere</Button> */}
-      </Card.Body>
-    </Card>
-      </div>
-    </div>
-    </div>
-  )
-}
+      <div className="row">
+      <div className="col-lg-12">
+      <Swiper 
+                     autoplay={{
+                       delay: 10500,
+                       pauseOnMouseEnter: true,
+                       disableOnInteraction: false
+                     }}
+              className="ServisSlide"
+              pagination={{          type: "fraction",
+              }}
+              modules={[Pagination]}
+              spaceBetween={30}
+              slidesPerView={4}
+              grabCursor ={true}
+              breakpoints={{
+                320: {
+                  slidesPerView: 1,
+                },
+                // when window width is >= 640px
+                640: {
+    
+                  slidesPerView: 1,
+                },
+                // when window width is >= 768px
+                768: {
+   
+                  slidesPerView: 2,
+                },
+                1200: {
+   
+                  slidesPerView: 3,
+                },
+                1500: {
 
-export default ServicesSection
+                  slidesPerView: 3,
+                },
+
+                1600: {
+
+                  slidesPerView: 4,
+                },
+
+              }}
+            
+            >
+              {data.data.map((item) => (
+       
+                <SwiperSlide className="d-flex justify-content-center swiper-slide " key={item.id}>
+                           <Link to ={item.attributes.CardUrl}>
+                   <Card>
+            <Card.Img
+              className="ymServicesimg1"
+              variant="top"
+              src={item.attributes.imageUrl}
+            />
+            <Card.Body>
+              <Card.Title>{item.attributes.CardHead}</Card.Title>
+              <Card.Text>
+              {item.attributes.CardText}
+              </Card.Text>
+
+            </Card.Body>
+          </Card>
+          </Link>
+                </SwiperSlide>
+       
+                
+              ))}
+
+
+
+            </Swiper>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
