ab8c3ad187cb858d0922ce59501741e41bc7ebb6
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2023-05-09 09:40:25 +0300

    RentalPage
    RentalPage

--- src/components/StudioRental.js
+++ src/components/StudioRental.js
@@ -1,18 +1,33 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import StudyoRentalSlider from "./section_components/seciton_in_components/StudyoRentalSlider";
 import { Helmet } from "react-helmet";
 const StudioRental = () => {
+  const { isLoading, error, data } = useFetch(
+    "http://localhost:1337/api/studyo-kiralama"
+  );
+  if (isLoading) return <h1>Yükleniyor...</h1>;
+  if (error) return <h1>Hata: {error.message}</h1>;
   return (
-    <section className="PageStudioRental">
+    <section className="PageStudioRental OtherPage">
       <Helmet>
         <title>Stüdyo Kirala</title>
       </Helmet>
-      <section className="headerGorseli">  
-  <div className="headerBaslik">
-     <div className="container">
-     <h1>Stüdyo Kirala</h1>
-     </div>
-  </div>
-</section>
+      <div className="container-fluid padrl">
+        <div className="row">
+          <div className="col-lg-12">
+            <div key={data.id} className="OtherPageSloganArea">
+              <h1>{data.data.attributes.RentalHeading}</h1>
+            </div>
+          </div>
+
+          <div className="col-lg-12">
+            <StudyoRentalSlider></StudyoRentalSlider>
+          </div>
+
+        </div>
+      </div>
+      
     </section>
   );
 };
