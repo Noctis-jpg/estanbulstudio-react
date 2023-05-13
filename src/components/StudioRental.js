c8c99865a1ab80be3de8285447761e2e7fd4da8a
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2023-05-13 15:48:18 +0300

    New

--- src/components/StudioRental.js
+++ src/components/StudioRental.js
@@ -4,7 +4,7 @@ import StudyoRentalSlider from "./section_components/seciton_in_components/Study
 import { Helmet } from "react-helmet";
 const StudioRental = () => {
   const { isLoading, error, data } = useFetch(
-    "http://localhost:1337/api/studyo-rental-ekipmen"
+    "http://localhost:1337/api/rental-studios"
   );
   if (isLoading) return <h1>Yükleniyor...</h1>;
   if (error) return <h1>Hata: {error.message}</h1>;
@@ -15,31 +15,31 @@ const StudioRental = () => {
       </Helmet>
       <div className="container-fluid padrl">
         <div className="row">
-
-
           <div className="col-lg-12 pad-0">
             <StudyoRentalSlider></StudyoRentalSlider>
           </div>
-          <div className="col-lg-12">
-            <div key={data.id} className="OtherPageSloganArea">
-              <h1>{data.data.attributes.RentalHeading}</h1>
-            </div>
-          </div>
-          {data.data.map((item) => (
-          <div key={item.id} className="col-lg-3" ddsdasd >
-          
-              <h2 >{item.attributes.HeadingEkipman}</h2>
-              <p >{item.attributes.EkipmanListItem}</p>
-            </div>
-     
-        ))}
         </div>
       </div>
       <div className="container">
         <div className="row">
-
-    </div>
-    </div>
+          {data.data.map((item) => (
+            <div key={item.id} className="col-lg-3">
+              <div className="HeadingRental">
+                <h2 className="header">
+                  {item.attributes.StudyoEkipmanBaslik}
+                </h2>
+                <ul>
+                  <li>{item.attributes.StudyoEkipmanicerik}</li>
+            <li>      {item.attributes.ListItems}</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="container">
+        <div className="row"></div>
+      </div>
     </section>
   );
 };
