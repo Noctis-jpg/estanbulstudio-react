6c813947513e95a517b32ac0bd43d03c58ced086
Yunus <52599021+Noctis-jpg@users.noreply.github.com>
2023-05-27 13:14:27 +0300

    React leaflet silindi.
    React leaflet silindi.

--- src/components/section_components/seciton_in_components/GoogleMap.js
+++ src/components/section_components/seciton_in_components/GoogleMap.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+
 
 const GoogleMap = () => {
     return (
